5207a2257a6ace79b2653924738104017825cf45
Sebastiano <sebastiano@Sebas-MacBook-Pro.local>
2016-04-07 00:03:37 +0200

    adding query on local

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $("#search").keyup(function() {
     console.log($(this).attr("alt").search); 
         var searchAttr = $(this).attr("alt");
         
-    if(searchAttr.toLowerCase().search(search.toLowerCase()) > 0 ) {
+    if(searchAttr.toLowerCase().search(search.toLowerCase()) > -1 ) {
         $(this).show();
         } 
     else {
@@ -28,6 +28,7 @@ $("#search").keyup(function() {
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
 var $caption = $("<p></p>");
+var selected;
 
 //An image to overlay
 $overlay.append($image);
@@ -41,27 +42,32 @@ $(".gallery").append($overlay);
 //Capture the click on a image link 
 $(".gallery a").click(function(event){
   event.preventDefault();
-  var imageLocation = $(this).attr("href");
-  //Update overlay with the image linked in the link
-  $image.attr("src", imageLocation);
+  
+  loadInfo($(this));
   
   //Show the overlay.
   $overlay.show();
-  
-  //Get child's alt attribute and set caption
-  var captionText = $(this).find('span').text();
-  $caption.text(captionText);
 });
+
+
+function loadInfo(item){
+    var imageLocation = item.attr("href");
+    //Update overlay with the image linked in the link
+    $image.attr("src", imageLocation);
+    
+    //Get child's span element text and set caption
+    var captionText = item.find('span').text();
+    $caption.text(captionText);
+    
+    //Guardamos el index de esta imagen como la seleccionada
+    selected = item.index();
+    
+}
 
 //When overlay is clicked hide it
 $overlay.click(function(){
   $overlay.hide();
 });
-
-
-
-//Problem: Miss the next and prev. buttons
-//Solution: Create a buttons
 
 
 
